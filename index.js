d5f6cf69b73497141f94266f81ddc009de1c9f79
ArtiolaDomiCaka <artioladomi@hotmail.com>
2021-12-16 00:34:32 +0100

    more solutions

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
 const state = {
-    store: []
+    store: [],
+    typeFilters: ['Girls', 'Guys', 'Sale'],
+    selectedFilter: 'Home',
+    selectedItem: null,
+    search: '',
+    modals: ''
 }
+
+function searchModal() {
+    //MODAL 
+    const divEl = document.createElement("div")
+    divEl.setAttribute("class", "model-wrapper")
+
+    const modalDivEl = document.createElement("div")
+    modalDivEl.setAttribute("class", "modal")
+
+    const buttonEl = document.createElement("button")
+    buttonEl.setAttribute("class", "modal__close-btn")
+    buttonEl.textContent = "x"
+    buttonEl.addEventListener("click", function () {
+        state.modals = ""
+        render()
+    })
+
+    const profileEl = document.createElement("h2")
+    profileEl.textContent = "What are you searching for?"
+
+    const inputEl = document.createElement("input")
+    inputEl.setAttribute("type", "text")
+    inputEl.setAttribute("class", "input-modal")
+    inputEl.setAttribute("name", "search-input")
+    inputEl.setAttribute("placeholder", "search")
+
+    modalDivEl.append(buttonEl, profileEl, inputEl)
+    divEl.append(modalDivEl)
+    document.body.append(divEl)
+
+}
+
+function renderModals() {
+    if (state.modals === 'search') {
+        searchModal()
+    }
+}
+
+function getItemsToDisplay() {
+
+    let itemsToDisplay = state.store
+    if (state.selectedFilter === 'Girls') {
+        itemsToDisplay = itemsToDisplay.filter(item => item.type === 'Girls')
+    }
+    if (state.selectedFilter === 'Guys') {
+        itemsToDisplay = itemsToDisplay.filter(item => item.type === 'Guys')
+    }
+    if (state.selectedFilter === 'Sale') {
+        itemsToDisplay = itemsToDisplay.filter(item => item.discountedPrice !== undefined)
+    }
+    return itemsToDisplay
+}
+// if (state.search !== ''){
+//     itemsToDisplay = itemsToDisplay.filter(item => item.name === )
+// }
+
+
+
 
 //SERVER FUNCTIONS
 
@@ -35,39 +98,37 @@ function renderHeader() {
     const headerEl = document.createElement("header")
     const h1El = document.createElement("h1")
     h1El.textContent = "HOLLIXTON"
+    h1El.addEventListener("click", function () {
+        state.selectedFilter = "Home"
+        state.selectedItem = null
+        render()
 
-    const headerNav = document.createElement("nav")
-    headerNav.setAttribute("class", "header-nav")
+    })
 
-    const ulEl = document.createElement("ul")
-    ulEl.setAttribute("class", "header-ul")
+    //Left 
+    const headerNavLeft = document.createElement("nav")
+    headerNavLeft.setAttribute("class", "header-nav")
 
-    const girlsHeaderList = document.createElement("li")
-    girlsHeaderList.setAttribute("class", "girls-item")
-    const anchorGirlsList = document.createElement("a")
-    anchorGirlsList.setAttribute("href", "null")
-    anchorGirlsList.textContent = "Girls"
-    girlsHeaderList.append(anchorGirlsList)
+    const leftUlEl = document.createElement("ul")
+    leftUlEl.setAttribute("class", "header-ul")
+
+    for (const filter of state.typeFilters) {
+        const liEl = document.createElement("li")
+        liEl.setAttribute("class", "header__left__item")
+        const aEl = document.createElement("a")
+        aEl.setAttribute("href", "#")
+        aEl.textContent = filter
+        aEl.addEventListener("click", function () {
+            state.selectedFilter = filter
+            state.selectedItem = null
+            render()
+        })
+        liEl.append(aEl)
+        headerNavLeft.append(liEl)
+
+    }
 
 
-    const guysHeaderList = document.createElement("li")
-    guysHeaderList.setAttribute("class", "guys-item")
-    const anchorGuysList = document.createElement("a")
-    anchorGuysList.setAttribute("href", "null")
-    anchorGuysList.textContent = "Guys"
-    guysHeaderList.append(anchorGuysList)
-
-    const saleHeaderList = document.createElement("li")
-    saleHeaderList.setAttribute("class", "sale-item")
-    const anchorItemList = document.createElement("a")
-    anchorItemList.setAttribute("href", "null")
-    anchorItemList.textContent = "Sale"
-    saleHeaderList.append(anchorItemList)
-
-    headerEl.append(headerNav)
-    headerNav.append(h1El, ulEl)
-    ulEl.append(girlsHeaderList, guysHeaderList, saleHeaderList)
-    console.log(headerEl)
 
     const rightSectionBar = document.createElement("section")
     rightSectionBar.setAttribute("class", "section-bar")
@@ -81,6 +142,11 @@ function renderHeader() {
 
     const searchLoopButton = document.createElement("button")
     searchLoopButton.setAttribute("class", "search-button")
+    searchLoopButton.addEventListener("click", function () {
+        state.modals = "search"
+        render()
+    })
+
     const imageSearchButton = document.createElement("img")
     imageSearchButton.setAttribute("src", "./assets/magnifiying-glass.png")
 
@@ -114,7 +180,7 @@ function renderHeader() {
 
     rightUlEl.append(searchLoop, loginLi, cartLi)
     rightSectionBar.append(rightUlEl)
-    headerEl.append(headerNav, rightSectionBar)
+    headerEl.append(h1El, headerNavLeft, rightSectionBar)
     document.body.append(headerEl)
 
 
@@ -125,6 +191,10 @@ function renderProductItem(product, productList) {
 
     const productEl = document.createElement("li")
     productEl.setAttribute("class", "product-item")
+    productEl.addEventListener("click", function () {
+        state.selectedItem = product
+        render()
+    })
 
     const imageEl = document.createElement("img")
     imageEl.setAttribute("class", "product-item__image")
@@ -166,21 +236,57 @@ function renderProductItem(product, productList) {
     productList.append(productEl)
 }
 
-function renderMain() {
-    const mainEl = document.createElement("main")
+function renderItemDetails(mainEl) {
+    const divEl = document.createElement("div")
+    divEl.setAttribute("class", "product-details")
+
+    const imgEl = document.createElement("img")
+    imgEl.setAttribute("class", "product-details__image")
+    imgEl.setAttribute("src", state.selectedItem.image)
+
+    const titleEl = document.createElement("h2")
+    titleEl.setAttribute("class", "product-details__title")
+    titleEl.textContent = state.selectedItem.name
+
+    const addToBagButton = document.createElement("button")
+    addToBagButton.setAttribute("class", "product-details__add-to-bag")
+    addToBagButton.textContent = "ADD TO BAG"
+    addToBagButton.addEventListener("click", function () {
+        state.selectedItem = null
+        render()
+    })
+
+    divEl.append(imgEl, titleEl, addToBagButton)
+    mainEl.append(divEl)
+}
+
+function renderProductList(mainEl) {
     const h2El = document.createElement("h2")
     h2El.setAttribute("class", "main-title")
-    h2El.textContent = "Home"
+    h2El.textContent = state.selectedFilter
     const productList = document.createElement("ul")
     productList.setAttribute("class", "product-list")
-    for (const product of state.store) {
+    for (const product of getItemsToDisplay()) {
         renderProductItem(product, productList)
 
     }
     mainEl.append(h2El, productList)
-    document.body.append(mainEl)
-
 }
+
+function renderMain() {
+    const mainEl = document.createElement("main")
+
+    if (state.selectedItem !== null) {
+        renderItemDetails(mainEl)
+    } else {
+
+        renderProductList(mainEl)
+    }
+    document.body.append(mainEl)
+}
+
+
+
 
 function renderFooter() {
     const footerEl = document.createElement("footer")
@@ -191,8 +297,7 @@ function renderFooter() {
 
     const stateFooterEl = document.createElement("h3")
     stateFooterEl.setAttribute("class", "state-footer")
-    stateFooterEl.textContent =
-        "United Kingdom"
+    stateFooterEl.textContent = "United Kingdom"
     footerEl.append(titleFooterEl, stateFooterEl)
     document.body.append(footerEl)
 }
@@ -202,6 +307,7 @@ function render() {
     renderHeader()
     renderMain()
     renderFooter()
+    renderModals()
 }
 
 function init() {
